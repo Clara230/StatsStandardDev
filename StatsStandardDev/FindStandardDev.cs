72adea628b92a7da5fb368a9dd2ccd6ceef128ad
clara9621 <58587888+clara9621@users.noreply.github.com>
2020-01-15 11:29:55 +0200

    Add project files.

---
+++ StatsStandardDev/FindStandardDev.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using StatsOperations;
+
+
+
+namespace StatsStandardDeviation
+
+{
+
+    public class FindStandardDev : IFindStandardDev
+
+    {
+
+        public static dynamic result;
+
+        public double StandardDev(dynamic arrayA)
+
+        {
+
+            result = StatsOperations.StatsStandardDev.StandardDev(arrayA);
+
+            return result;
+
+        }
+
+    }
+
+}
